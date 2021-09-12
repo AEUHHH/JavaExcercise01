fb108654ef33084fe9230d4c8ae13ba51daebaca
AEUHHH <dr.codycash@gmail.com>
2021-09-11 22:32:42 -0400

    Update01

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class App
+
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Scanner read = new Scanner(System.in);
+        String str = read.next();
+    }
+
+
+
+
+
+}
